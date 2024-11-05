1a8ca0d5e4aae0a706c32c5f2e49a93c42a04224
Elnur Hasanov <71289037+thehasanovv@users.noreply.github.com>
2024-11-05 09:10:17 +0400

    liquibase added

---
+++ src/test/java/com/example/springdatarediscache/SpringDataRedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatarediscache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRedisCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
